bab448b9e08d47ba76e7c6b021f124e75dbdbae1
kursadguzelkaya <kursad.gzlky@gmail.com>
2022-11-09 17:12:12 +0300

    fix(Login): update logInRequest with parameters

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { func } from 'prop-types';
 import { Link, useNavigate } from 'react-router-dom';
 
 
 const Login = ({ logInRequest }) => {
-  console.log("login page");
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
 
   const navigate = useNavigate();
+
+  const logIn = () => {
+    const user = {
+      email,
+      password
+    }
+    logInRequest(user, navigate);
+  }
   
   return (
     <div className="login">
       <div>ErasMOVE</div>
-        <input id="username" className='inp' placeholder='username'></input>
-        <input type={"password"} id="password" className='inp' placeholder='password'></input>
-        <button className='login-btn' onClick={() => logInRequest(navigate)}>Login</button> 
+        <input id="email" className='inp' placeholder='email' value={email} onChange={e => setEmail(e.target.value)}></input>
+        <input type={"password"} id="password" className='inp' placeholder='password' value={password} onChange={e => setPassword(e.target.value)}></input>
+        <button className='login-btn' onClick={() => logIn()}>Login</button> 
         <p> Did you forgot your password? <Link to="/forgotPassword">Forgot Password</Link></p>
     </div>
   );
