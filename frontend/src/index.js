d7e69ecff4877f64e74119a765db51b5b9b83344
Gökhan Tekin <sgtekin7@gmail.com>
2022-11-11 17:19:10 +0300

    bootstrap added //index.js popper not being used problem exists

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createStore, applyMiddleware, compose } from 'redux';
