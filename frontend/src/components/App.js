541e8d3286bff114521eed69c0d1cfad721c89a3
Gökhan Tekin <sgtekin7@gmail.com>
2022-11-11 16:21:25 +0300

    Auto stash before merge of "withTemplate" and "origin/withTemplate"

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline, StyledEngineProvider } from '@mui/material';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, StyledEngineProvider } from "@mui/material";
 
+<<<<<<< Updated upstream
 import Routes from '../routes';
 import themes from '../themes';
+=======
+import Routes from "./routes/mainRouter";
+import themes from "../themes";
+>>>>>>> Stashed changes
 // import NavigationScroll from '../layout/NavigationScroll';
 
 const App = () => {
-    const customization = useSelector(state => state.customization);
+  const customization = useSelector(state => state.customization);
 
-    return (
-        <StyledEngineProvider injectFirst>
-            <ThemeProvider theme={ themes(customization) }>
-              <CssBaseline />
-              <Router>
-                <Routes />
-              </Router>
-            </ThemeProvider>
-        </StyledEngineProvider>
-    );
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themes(customization)}>
+        <CssBaseline />
+        <Router>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
 };
 
 export default App;
