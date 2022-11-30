54f81735eb3c467262274c50fdfb0d88af75ea90
HP <alperengozeten@gmail.com>
2022-11-30 14:03:05 +0300

    backend start

---
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/ErasMoveApplication.java
@@ -0,0 +1,13 @@
+package com.erasmuarrem.ErasMove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasMoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErasMoveApplication.class, args);
+	}
+
+}
---
+++ backend/ErasMove/src/test/java/com/erasmuarrem/ErasMove/ErasMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erasmuarrem.ErasMove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasMoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
