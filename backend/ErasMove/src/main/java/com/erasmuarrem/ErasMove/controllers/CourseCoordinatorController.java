00ffeab49c7ad699edd7894d071b07c9090a9d75
HP <alperengozeten@gmail.com>
2022-12-04 20:26:10 +0300

    feat (ElectiveCourseApprovalRequest): search by student and department coordinator added

---
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/controllers/CourseCoordinatorController.java
@@ -0,0 +1,45 @@
+package com.erasmuarrem.ErasMove.controllers;
+
+import com.erasmuarrem.ErasMove.models.CourseCoordinator;
+import com.erasmuarrem.ErasMove.services.CourseCoordinatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courseCoordinator")
+public class CourseCoordinatorController {
+
+    private final CourseCoordinatorService courseCoordinatorService;
+
+    @Autowired
+    public CourseCoordinatorController(CourseCoordinatorService courseCoordinatorService) {
+        this.courseCoordinatorService = courseCoordinatorService;
+    }
+
+    @GetMapping()
+    public List<CourseCoordinator> getCourseCoordinators() {
+        return courseCoordinatorService.getCourseCoordinators();
+    }
+
+    @GetMapping("/{id}")
+    public CourseCoordinator getCourseCoordinatorByID(@PathVariable("id") Long id) {
+        return courseCoordinatorService.getCourseCoordinatorByID(id);
+    }
+
+    @GetMapping("/course/{id}")
+    public CourseCoordinator getCourseCoordinatorByCourseID(@PathVariable("id") Long id) {
+        return courseCoordinatorService.getCourseCoordinatorByCourseID(id);
+    }
+
+    @PostMapping("/add")
+    public void addCourseCoordinator(@RequestBody CourseCoordinator courseCoordinator) {
+        courseCoordinatorService.addCourseCoordinator(courseCoordinator);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCourseCoordinatorByID(@PathVariable("id") Long id) {
+        courseCoordinatorService.deleteCourseCoordinatorByID(id);
+    }
+}
