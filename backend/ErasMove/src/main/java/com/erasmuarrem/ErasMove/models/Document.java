9de8e6605654f38c5b08e273ea6bf30e758582d7
HP <alperengozeten@gmail.com>
2022-12-05 19:30:41 +0300

    fix (Request, Document): Document now holds the Request ID and Request ID generation changed to sequence

---
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/models/Document.java
@@ -0,0 +1,23 @@
+package com.erasmuarrem.ErasMove.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    private Long requestID;
+
+    private String documentName;
+    private String description;
+}
