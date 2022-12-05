af2092188261eb6ac088cb715fea922197e2c99c
HP <alperengozeten@gmail.com>
2022-12-05 11:45:01 +0300

    feat (Language): Language model + controller + service + repository

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/models/OutgoingStudent.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/models/OutgoingStudent.java
@@ -1,13 +1,10 @@
 package com.erasmuarrem.ErasMove.models;
 
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @Entity
 @Table
@@ -18,7 +15,5 @@ public class OutgoingStudent extends Student {
     private double cgpa;
     private Boolean isDoubleMajor;
 
-    @ElementCollection
-    private List<String> proficientLanguages;
     // private Application application;
 }
