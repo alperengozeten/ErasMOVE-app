c926227dd035771d24850d3e14b66d29572d6649
HP <alperengozeten@gmail.com>
2022-12-06 18:32:38 +0300

    feat (Department): add course, delete course by id and name

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/services/DepartmentService.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/services/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.erasmuarrem.ErasMove.services;
 
+import com.erasmuarrem.ErasMove.models.Course;
 import com.erasmuarrem.ErasMove.models.Department;
+import com.erasmuarrem.ErasMove.repositories.CourseRepository;
 import com.erasmuarrem.ErasMove.repositories.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,12 @@ import java.util.Optional;
 public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
+    private final CourseRepository courseRepository;
 
     @Autowired
-    public DepartmentService(DepartmentRepository departmentRepository) {
+    public DepartmentService(DepartmentRepository departmentRepository, CourseRepository courseRepository) {
         this.departmentRepository = departmentRepository;
+        this.courseRepository = courseRepository;
     }
 
     public List<Department> getDepartments() {
@@ -63,5 +67,81 @@ public class DepartmentService {
         }
 
         departmentRepository.deleteById(id);
+    }
+
+    public void addCourseByDepartmentID(Course course, Long id) {
+        Optional<Department> departmentOptional = departmentRepository.findById(id);
+
+        if ( !departmentOptional.isPresent() ) {
+            throw new IllegalStateException("Department with id:" + id + " doesn't exist!");
+        }
+
+        Department department = departmentOptional.get();
+        List<Course> courseList = department.getCourseList();
+
+        for (Course departmentCourse : courseList) {
+            if ( departmentCourse.getCourseName().equals(course.getCourseName()) ) {
+                throw new IllegalStateException("Course with name:" + course.getCourseName() + " already exists in this department!");
+            }
+        }
+
+        courseList.add(course);
+        courseRepository.save(course);
+        departmentRepository.save(department);
+    }
+
+    public void deleteCourseByDepartmentIDAndCourseID(Long id, Long courseID) {
+        Optional<Department> departmentOptional = departmentRepository.findById(id);
+
+        if ( !departmentOptional.isPresent() ) {
+            throw new IllegalStateException("Department with id:" + id + " doesn't exist!");
+        }
+
+        Optional<Course> courseOptional = courseRepository.findById(courseID);
+
+        if ( !courseOptional.isPresent() ) {
+            throw new IllegalStateException("Course with id:" + courseID + " doesn't exist!");
+        }
+
+        Department department = departmentOptional.get();
+        List<Course> courseList = department.getCourseList();
+        Course course = courseOptional.get();
+
+        if ( !courseList.contains(course) ) {
+            throw new IllegalStateException("Course with id:" + courseID + " isn't in the department!");
+        }
+
+        courseList.remove(course); // update the list
+        courseRepository.deleteById(courseID); // delete the course
+        departmentRepository.save(department); // save the department
+    }
+
+    public void deleteCourseByDepartmentIDAndCourseName(Long id, String courseName) {
+        Optional<Department> departmentOptional = departmentRepository.findById(id);
+
+        if ( !departmentOptional.isPresent() ) {
+            throw new IllegalStateException("Department with id:" + id + " doesn't exist!");
+        }
+
+        Department department = departmentOptional.get();
+        List<Course> courseList = department.getCourseList();
+        boolean courseNameExists = false;
+        Course foundCourse = null;
+
+        for (Course course : courseList) {
+            if ( course.getCourseName().equals(courseName) ) {
+                courseNameExists = true;
+                foundCourse = course;
+                break;
+            }
+        }
+
+        if ( !courseNameExists ) {
+            throw new IllegalStateException("Course with name:" + courseName + " doesn't exist in the department!");
+        }
+
+        courseList.remove(foundCourse);
+        courseRepository.deleteById(foundCourse.getID());
+        departmentRepository.save(department);
     }
 }
