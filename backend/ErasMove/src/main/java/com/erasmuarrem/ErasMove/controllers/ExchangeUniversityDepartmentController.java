57b00bcc7ba8f30c3d64517173fffc2eb172e725
HP <alperengozeten@gmail.com>
2022-12-06 22:56:33 +0300

    feat (ExchangeUniversityDepartment): add + delete courses

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/controllers/ExchangeUniversityDepartmentController.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/controllers/ExchangeUniversityDepartmentController.java
@@ -1,5 +1,6 @@
 package com.erasmuarrem.ErasMove.controllers;
 
+import com.erasmuarrem.ErasMove.models.Course;
 import com.erasmuarrem.ErasMove.models.ExchangeUniversityDepartment;
 import com.erasmuarrem.ErasMove.services.ExchangeUniversityDepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,23 @@ public class ExchangeUniversityDepartmentController {
         exchangeUniversityDepartmentService.addExchangeUniversityDepartment(exchangeUniversityDepartment);
     }
 
+    @PostMapping("/addCourse/{id}")
+    public void addCourseByExchangeDepartmentID(@RequestBody Course course, @PathVariable("id") Long id) {
+        exchangeUniversityDepartmentService.addCourseByExchangeDepartmentID(course, id);
+    }
+
     @DeleteMapping("/delete/{id}")
     public void deleteExchangeUniversityDepartmentByID(@PathVariable("id") Long id) {
         exchangeUniversityDepartmentService.deleteExchangeUniversityDepartmentByID(id);
+    }
+
+    @DeleteMapping("/delete/{id}/course/{courseID}")
+    public void deleteCourseByExchangeDepartmentIDAndCourseID(@PathVariable("id") Long id, @PathVariable("courseID") Long courseID) {
+        exchangeUniversityDepartmentService.deleteCourseByExchangeDepartmentIDAndCourseID(id, courseID);
+    }
+
+    @DeleteMapping("/delete/{id}/courseName/{courseName}")
+    public void deleteCourseByExchangeDepartmentIDAndCourseName(@PathVariable("id") Long id, @PathVariable("courseName") String courseName) {
+        exchangeUniversityDepartmentService.deleteCourseByExchangeDepartmentIDAndCourseName(id, courseName);
     }
 }
--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/services/ExchangeUniversityDepartmentService.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/services/ExchangeUniversityDepartmentService.java
@@ -1,6 +1,8 @@
 package com.erasmuarrem.ErasMove.services;
 
+import com.erasmuarrem.ErasMove.models.Course;
 import com.erasmuarrem.ErasMove.models.ExchangeUniversityDepartment;
+import com.erasmuarrem.ErasMove.repositories.CourseRepository;
 import com.erasmuarrem.ErasMove.repositories.ExchangeUniversityDepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,12 @@ import java.util.Optional;
 public class ExchangeUniversityDepartmentService {
 
     private final ExchangeUniversityDepartmentRepository exchangeUniversityDepartmentRepository;
+    private final CourseRepository courseRepository;
 
     @Autowired
-    public ExchangeUniversityDepartmentService(ExchangeUniversityDepartmentRepository exchangeUniversityDepartmentRepository) {
+    public ExchangeUniversityDepartmentService(ExchangeUniversityDepartmentRepository exchangeUniversityDepartmentRepository, CourseRepository courseRepository) {
         this.exchangeUniversityDepartmentRepository = exchangeUniversityDepartmentRepository;
+        this.courseRepository = courseRepository;
     }
 
     public List<ExchangeUniversityDepartment> getExchangeUniversityDepartments() {
@@ -55,5 +59,84 @@ public class ExchangeUniversityDepartmentService {
         }
 
         exchangeUniversityDepartmentRepository.deleteById(id);
+    }
+
+    public void addCourseByExchangeDepartmentID(Course course, Long id) {
+        Optional<ExchangeUniversityDepartment> exchangeUniversityDepartmentOptional = exchangeUniversityDepartmentRepository
+                .findById(id);
+
+        if ( !exchangeUniversityDepartmentOptional.isPresent() ) {
+            throw new IllegalStateException("Exchange University Department with id:" + id + " doesn't exist!");
+        }
+
+        ExchangeUniversityDepartment exchangeUniversityDepartment = exchangeUniversityDepartmentOptional.get();
+        List<Course> courseList = exchangeUniversityDepartment.getCourseList();
+
+        for (Course departmentCourse : courseList) {
+            if ( departmentCourse.getCourseName().equals(course.getCourseName()) ) {
+                throw new IllegalStateException("Course with name:" + course.getCourseName() + " already exists in this department!");
+            }
+        }
+
+        courseList.add(course);
+        courseRepository.save(course);
+        exchangeUniversityDepartmentRepository.save(exchangeUniversityDepartment);
+    }
+
+    public void deleteCourseByExchangeDepartmentIDAndCourseID(Long id, Long courseID) {
+        Optional<ExchangeUniversityDepartment> exchangeUniversityDepartmentOptional = exchangeUniversityDepartmentRepository
+                .findById(id);
+
+        if ( !exchangeUniversityDepartmentOptional.isPresent() ) {
+            throw new IllegalStateException("Exchange University Department with id:" + id + " doesn't exist!");
+        }
+
+        Optional<Course> courseOptional = courseRepository.findById(courseID);
+
+        if ( !courseOptional.isPresent() ) {
+            throw new IllegalStateException("Course with id:" + courseID + " doesn't exist!");
+        }
+
+        ExchangeUniversityDepartment exchangeUniversityDepartment = exchangeUniversityDepartmentOptional.get();
+        List<Course> courseList = exchangeUniversityDepartment.getCourseList();
+        Course course = courseOptional.get();
+
+        if ( !courseList.contains(course) ) {
+            throw new IllegalStateException("Course with id:" + courseID + " doesn't exist in erasmus department!");
+        }
+
+        courseList.remove(course);
+        courseRepository.deleteById(courseID);
+        exchangeUniversityDepartmentRepository.save(exchangeUniversityDepartment);
+    }
+
+    public void deleteCourseByExchangeDepartmentIDAndCourseName(Long id, String courseName) {
+        Optional<ExchangeUniversityDepartment> exchangeUniversityDepartmentOptional = exchangeUniversityDepartmentRepository
+                .findById(id);
+
+        if ( !exchangeUniversityDepartmentOptional.isPresent() ) {
+            throw new IllegalStateException("Exchange University Department with id:" + id + " doesn't exist!");
+        }
+
+        ExchangeUniversityDepartment exchangeUniversityDepartment = exchangeUniversityDepartmentOptional.get();
+        List<Course> courseList = exchangeUniversityDepartment.getCourseList();
+        boolean courseNameExists = false;
+        Course foundCourse = null;
+
+        for (Course course : courseList) {
+            if ( course.getCourseName().equals(courseName) ) {
+                courseNameExists = true;
+                foundCourse = course;
+                break;
+            }
+        }
+
+        if ( !courseNameExists ) {
+            throw new IllegalStateException("Course with name:" + courseName + " doesn't exist in the erasmus university department!");
+        }
+
+        courseList.remove(foundCourse);
+        courseRepository.deleteById(foundCourse.getID());
+        exchangeUniversityDepartmentRepository.save(exchangeUniversityDepartment);
     }
 }
