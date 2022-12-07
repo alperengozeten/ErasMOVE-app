88915e08391cc7c13a50687f2c4c725f6771aaa0
HP <alperengozeten@gmail.com>
2022-12-07 21:12:02 +0300

    fix (FileRequest): controller + model + service + repository

---
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/services/FileRequestService.java
@@ -0,0 +1,63 @@
+package com.erasmuarrem.ErasMove.services;
+
+import com.erasmuarrem.ErasMove.models.FileRequest;
+import com.erasmuarrem.ErasMove.repositories.AdministrativeStaffRepository;
+import com.erasmuarrem.ErasMove.repositories.FileRequestRepository;
+import com.erasmuarrem.ErasMove.repositories.OutgoingStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FileRequestService {
+
+    private final FileRequestRepository fileRequestRepository;
+    private final OutgoingStudentRepository outgoingStudentRepository;
+    private final AdministrativeStaffRepository administrativeStaffRepository;
+
+    @Autowired
+    public FileRequestService(FileRequestRepository fileRequestRepository, OutgoingStudentRepository outgoingStudentRepository, AdministrativeStaffRepository administrativeStaffRepository) {
+        this.fileRequestRepository = fileRequestRepository;
+        this.outgoingStudentRepository = outgoingStudentRepository;
+        this.administrativeStaffRepository = administrativeStaffRepository;
+    }
+
+    public List<FileRequest> getFileRequests() {
+        return fileRequestRepository.findAll();
+    }
+
+    public FileRequest getFileRequestByID(Long id) {
+        Optional<FileRequest> fileRequestOptional = fileRequestRepository.findById(id);
+
+        if ( !fileRequestOptional.isPresent() ) {
+            throw new IllegalStateException("File Request with id:" + id + " doesn't exist!");
+        }
+
+        return fileRequestOptional.get();
+    }
+
+    public void addFileRequest(FileRequest fileRequest) {
+        Long administrativeStaffID = fileRequest.getAdministrativeStaff().getID();
+        Long outgoingStudentID = fileRequest.getStudent().getID();
+
+        if ( !administrativeStaffRepository.existsById(administrativeStaffID) ) {
+            throw new IllegalStateException("Administrative Staff with id:" + administrativeStaffID + " doesn't exist!");
+        }
+
+        if ( !outgoingStudentRepository.existsById(outgoingStudentID) ) {
+            throw new IllegalStateException("Outgoing Student with id:" + outgoingStudentID + " doesn't exist!");
+        }
+
+        fileRequestRepository.save(fileRequest);
+    }
+
+    public void deleteFileRequestByID(Long id) {
+        if ( !fileRequestRepository.existsById(id) ) {
+            throw new IllegalStateException("File Request with id:" + id + " doesn't exist!");
+        }
+
+        fileRequestRepository.deleteById(id);
+    }
+}
