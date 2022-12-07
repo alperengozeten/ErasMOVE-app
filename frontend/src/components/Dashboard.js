f0cbbd57aedcb571043f94e74acdec6831725edc
Kürşad Güzelkaya <55952069+kursadguzelkaya0@users.noreply.github.com>
2022-12-08 00:52:28 +0300

    feat(saveComponentAsPDF): add a button to save given component as pdf

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,12 +1,20 @@
+import { Box, Stack } from '@mui/system';
 import React from 'react';
 import ExcelReader from './ExcelReader';
+import SaveComponentAsPDFButton from './SaveComponentAsPDFButton';
+const Foo = <b>foo</b>;
+
 
 const Dashboard = () => {
-  console.log('Dashboard Page');
 
   return (
     <div className="dashboard">
-        <ExcelReader /> 
+        <Stack spacing={2}>
+          <ExcelReader /> 
+          <Box>
+            <SaveComponentAsPDFButton component={Foo} pdfName={"foo"} />
+          </Box>
+        </Stack>
     </div>);
 };
 
