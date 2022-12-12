c64f5ea813fa88910ee3be577aab4403f6839cfd
Kürşad Güzelkaya <55952069+kursadguzelkaya0@users.noreply.github.com>
2022-12-12 23:07:46 +0300

    fix(createRequestDetail): update modal view

--- frontend/src/components/Application/CourseRequests.js
+++ frontend/src/components/Application/CourseRequests.js
@@ -1,4 +1,4 @@
-import { Box, Button, FormControl, FormControlLabel, Grid, InputLabel, MenuItem, Modal, Radio, RadioGroup, Select, Stack, TextField, Typography } from '@mui/material';
+import { Box, Button, FormControl, FormControlLabel, Grid, MenuItem, Modal, Radio, RadioGroup, Select, Stack, TextField, Typography } from '@mui/material';
 import NoteAddIcon from '@mui/icons-material/NoteAdd';
 import SendIcon from '@mui/icons-material/Send';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 
 import { sendReplacementOffer } from '../../actions';
 import CourseRequestTableForStudents from '../table/CourseRequestTableForStudents';
+import { MDBCard, MDBCardBody, MDBCardText, MDBCol, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
 
 const CourseRequests = ({ deleteCourseApprovalRequestRequest, courseRequests }) => {
     const [open, setOpen] = React.useState(false);
@@ -14,6 +15,10 @@ const CourseRequests = ({ deleteCourseApprovalRequestRequest, courseRequests }) 
     const [departmentValue, setDepartmentValue] = React.useState(0);
     const [courseValue, setCourseValue] = React.useState(0);
     const [isElective, setIsElective] = React.useState(false);
+    const [courseName, setCourseName] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [ects, setEcts] = React.useState(0);
+
 
 
     const handleOpen = () => setOpen(true);
@@ -27,6 +32,12 @@ const CourseRequests = ({ deleteCourseApprovalRequestRequest, courseRequests }) 
     };
     
     const handleCourseChange = e => setCourseValue(e.target.value);
+
+    const handleCourseNameChange = e => setCourseName(e.target.value);
+
+    const handleDescriptionChange = e => setDescription(e.target.value);
+
+    const handleEctsChange = e => setEcts(e.target.value);
 
     const handleElectiveChange = e => setIsElective(e.target.value);
 
@@ -52,94 +63,150 @@ const CourseRequests = ({ deleteCourseApprovalRequestRequest, courseRequests }) 
                                     variant="h2" component="h1">
                                     Create Course Request
                                 </Typography>
-                                <Stack alignItems={"center"} spacing={3}>
-                                    <Grid container spacing={2} width={"90%"} >
-                                        <Grid item xs={6} >
-                                            <Stack spacing={4   }>
-                                                <Box alignItems={"center"}>
-                                                    <TextField size='small' id="outlined-basic" label="Course Name" variant="outlined" />   
-                                                </Box>
-                                                <Box alignItems={"center"}>
-                                                    <TextField size="small" id="outlined-basic" label="Description" variant="outlined" />
-                                                </Box>
-                                            </Stack>
-                                        </Grid>
-                                        <Grid item xs={6}>
-                                            <Stack spacing={2}>
-                                                <Box>
-                                                    <FormControl  sx={{ m: 1, minWidth: 160 }}>
-                                                        <InputLabel id="demo-simple-select-label">Department</InputLabel>
-                                                        <Select
-                                                            required
-                                                            labelId="demo-simple-select-label"
-                                                            id="demo-simple-select"
-                                                            value={departmentValue}
-                                                            size="small"
-                                                            label="Department"
-                                                            onChange={handleDepartmentChange}
-                                                        >
-                                                            <MenuItem disabled value={0}>Select</MenuItem>
-                                                            <MenuItem value={10}>Computer Science</MenuItem>
-                                                            <MenuItem value={20}>Electrical Engineering</MenuItem>
-                                                            <MenuItem value={30}>Mechanical Engineering</MenuItem>
-                                                        </Select>
-                                                    </FormControl>       
-                                                </Box>
-                                                
-                                                <FormControl>
-                                                    <RadioGroup
-                                                        row
-                                                        aria-labelledby="demo-radio-buttons-group-label"
-                                                        defaultValue={false}
-                                                        name="radio-buttons-group"
-                                                        value={isElective}
-                                                        onChange={handleElectiveChange}
-                                                    >
-                                                        <FormControlLabel value={false} control={<Radio />} label="Must" />
-                                                        <FormControlLabel value={true} control={<Radio />} label="Elective" />
-                                                    </RadioGroup>
-                                                </FormControl>
-                                            </Stack>
-                                        </Grid>
+                            <Stack alignItems={"center"} spacing={3}>
+                                <section style={{ width: '100%', backgroundColor: '#eee' }}>
+                                    <MDBContainer className="py-5">
+                                    <MDBCard className="mb-4">
+                                    <MDBCardBody>
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Course Name</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <TextField
+                                                id="outlined-multiline-flexible"
+                                                value={courseName}
+                                                onChange={handleCourseNameChange}
+                                            />       
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Description</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <TextField
+                                                id="outlined-multiline-flexible"
+                                                fullWidth
+                                                value={description}
+                                                onChange={handleDescriptionChange}
+                                            />     
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>ECTS</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <TextField
+                                                id="outlined-multiline-flexible"
+                                                type={"number"}
+                                                value={ects}
+                                                onChange={handleEctsChange}
+                                            />     
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Type</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <FormControl>
+                                                <RadioGroup
+                                                    row
+                                                    aria-labelledby="demo-radio-buttons-group-label"
+                                                    defaultValue={false}
+                                                    name="radio-buttons-group"
+                                                    value={isElective}
+                                                    onChange={handleElectiveChange}
+                                                >
+                                                    <FormControlLabel value={false} control={<Radio />} label="Must" />
+                                                    <FormControlLabel value={true} control={<Radio />} label="Elective" />
+                                                </RadioGroup>
+                                            </FormControl>
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Department</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <FormControl  sx={{ m: 1, minWidth: 250 }}>
+                                                <Select
+                                                    required
+                                                    labelId="demo-simple-select-label"
+                                                    id="demo-simple-select"
+                                                    value={departmentValue}
+                                                    size="small"
+                                                    onChange={handleDepartmentChange}
+                                                >
+                                                    <MenuItem disabled value={0}>Select</MenuItem>
+                                                    <MenuItem value={10}>Computer Science</MenuItem>
+                                                    <MenuItem value={20}>Electrical Engineering</MenuItem>
+                                                    <MenuItem value={30}>Mechanical Engineering</MenuItem>
+                                                </Select>
+                                            </FormControl>   
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Equivalent Course</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <FormControl  sx={{ m: 1, minWidth: 140 }}>
+                                                <Select
+                                                    labelId="demo-simple-select-label"
+                                                    id="demo-simple-select"
+                                                    value={courseValue}
+                                                    size="small"
+                                                    onChange={handleCourseChange}
+                                                    disabled={departmentValue ? false : true }
+                                                >
+                                                    <MenuItem disabled value={0}>Select</MenuItem>
+                                                    <MenuItem value={10}>CS319</MenuItem>
+                                                    <MenuItem value={20}>CS315</MenuItem>
+                                                    <MenuItem value={30}>CS102</MenuItem>
+                                                </Select>
+                                            </FormControl>   
+                                        </MDBCol>
+                                        </MDBRow>
+                                        <hr />
+                                        <MDBRow>
+                                        <MDBCol sm="3">
+                                            <MDBCardText>Syllabus</MDBCardText>
+                                        </MDBCol>
+                                        <MDBCol sm="9">
+                                            <Button startIcon={<UploadFileIcon />} variant="contained" component="label">
+                                                Upload Syllabus
+                                                <input hidden accept="image/*" multiple type="file" />
+                                            </Button>
+                                        </MDBCol>
+                                        </MDBRow>
+                                    </MDBCardBody>
+                                    </MDBCard>
 
-                                        <Grid container sx={{marginTop: '10px'}} justifyContent={'center'} spacing={2}>
-                                            <Grid item xs={6}>
-                                                <Box>
-                                                    <FormControl  sx={{ m: 1, minWidth: 160 }}>
-                                                        <InputLabel id="demo-simple-select-label">Equivalent Course</InputLabel>
-                                                        <Select
-                                                            labelId="demo-simple-select-label"
-                                                            id="demo-simple-select"
-                                                            value={courseValue}
-                                                            size="small"
-                                                            label="Equivalent Course"
-                                                            onChange={handleCourseChange}
-                                                            disabled={departmentValue ? false : true }
-                                                        >
-                                                            <MenuItem disabled value={0}>Select</MenuItem>
-                                                            <MenuItem value={10}>CS319</MenuItem>
-                                                            <MenuItem value={20}>CS315</MenuItem>
-                                                            <MenuItem value={30}>CS102</MenuItem>
-                                                        </Select>
-                                                    </FormControl>       
-                                                </Box>
-                                            </Grid>
-                                            <Grid item xs={6}>
-                                                <Button startIcon={<UploadFileIcon />} variant="contained" component="label">
-                                                    Upload Syllabus
-                                                    <input hidden accept="image/*" multiple type="file" />
-                                                </Button>
-                                            </Grid>
+                                     
+                                        </MDBContainer>
+                                    </section>
+                                    <Grid container justifyContent={"center"}>
+                                        <Grid item xs={3}></Grid>
+                                        <Grid item xs={4}>
+                                            <Button sx={{margin: 'auto'}} variant="contained" color="success" size="medium" startIcon={<SendIcon />} onClick={handleClose} >
+                                                Create 
+                                            </Button>
                                         </Grid>
+                                        <Grid item xs={4}>
+                                            <Button sx={{margin: 'auto'}} variant="contained" color="error" size="medium" onClick={handleClose} >
+                                                Close
+                                            </Button>
+                                        </Grid>
+                                        <Grid item xs={1}></Grid>
                                     </Grid>
-                                    
-                                   
-                                    
-                                </Stack>
-                                <Stack alignItems={"flex-end"}>
-                                    <Button sx={{margin: 'auto'}} variant="contained" color="success" size="medium" startIcon={<SendIcon />} onClick={handleOpen} >
-                                        Create New Course Request
-                                    </Button>
                                 </Stack>
                             </Stack>
                         </Box>
@@ -162,6 +229,12 @@ const style = {
     borderRadius: "6px",
     boxShadow: 24,
     p: 4,
+    maxHeight: "90%",
+    mb: 2,
+    display: "flex",
+    flexDirection: "column",
+    overflow: "hidden",
+    overflowY: "scroll",
 };
 
 
