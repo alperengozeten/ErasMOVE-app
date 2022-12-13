7868bc25f1f8f775806b6d40b4070f42b5e1e9e5
Gökhan Tekin <sgtekin7@gmail.com>
2022-12-13 18:14:48 +0300

    fix(NotificationList): unused items deleted

--- frontend/src/layout/MainLayout/Header/NotificationSection/NotificationList.js
+++ frontend/src/layout/MainLayout/Header/NotificationSection/NotificationList.js
@@ -2,75 +2,16 @@
 import React from "react";
 import { useTheme, styled } from "@mui/material/styles";
 import {
-  Avatar,
-  Button,
-  Card,
-  CardContent,
-  Chip,
-  Divider,
-  Grid,
   List,
-  ListItem,
-  ListItemAvatar,
-  ListItemSecondaryAction,
-  ListItemText,
-  Stack,
-  Typography,
 } from "@mui/material";
 
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import NotificationCard from "../../../../components/table/notification/NotificationCard";
 
-// assets
-import {
-  IconBrandTelegram,
-  IconBuildingStore,
-  IconMailbox,
-  IconPhoto,
-} from "@tabler/icons";
-import User1 from "../../../../assets/images/users/user-round.svg";
-
-// styles
-const ListItemWrapper = styled("div")(({ theme }) => ({
-  cursor: "pointer",
-  padding: 16,
-  "&:hover": {
-    background: theme.palette.primary.light,
-  },
-  "& .MuiListItem-root": {
-    padding: 0,
-  },
-}));
-
-// ==============================|| NOTIFICATION LIST ITEM ||============================== //
 
 const NotificationList = ({ notifications }) => {
   const theme = useTheme();
-
-  const chipSX = {
-    height: 24,
-    padding: "0 6px",
-  };
-  const chipErrorSX = {
-    ...chipSX,
-    color: theme.palette.orange.dark,
-    backgroundColor: theme.palette.orange.light,
-    marginRight: "5px",
-  };
-
-  const chipWarningSX = {
-    ...chipSX,
-    color: theme.palette.warning.dark,
-    backgroundColor: theme.palette.warning.light,
-  };
-
-  const chipSuccessSX = {
-    ...chipSX,
-    color: theme.palette.success.dark,
-    backgroundColor: theme.palette.success.light,
-    height: 28,
-  };
 
   return (
     <List
