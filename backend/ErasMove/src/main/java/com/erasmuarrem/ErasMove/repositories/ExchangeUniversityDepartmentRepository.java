3d4eb755e9db24a499f77a3ab5025ba36d9c8b22
HP <alperengozeten@gmail.com>
2022-12-18 00:47:30 +0300

    feat (ErasmusUniversityDepartment, ExchangeUniversityDepartment): find departments by the university id

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/repositories/ExchangeUniversityDepartmentRepository.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/repositories/ExchangeUniversityDepartmentRepository.java
@@ -4,9 +4,11 @@ import com.erasmuarrem.ErasMove.models.ExchangeUniversityDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExchangeUniversityDepartmentRepository extends JpaRepository<ExchangeUniversityDepartment, Long> {
     Optional<ExchangeUniversityDepartment> findByExchangeUniversityIDAndDepartmentName(Long exchangeUniversityID, String departmentName);
+    List<ExchangeUniversityDepartment> findByExchangeUniversity_ID(Long exchangeUniversityID);
 }
