6390763dd2bd1c95cb7baf4261d27be9a76cdb09
HP <alperengozeten@gmail.com>
2022-12-18 23:35:49 +0300

    fix (ErasMoveApplication): configuration

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/ErasMoveApplication.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/ErasMoveApplication.java
@@ -2,12 +2,35 @@ package com.erasmuarrem.ErasMove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ErasMoveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ErasMoveApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		//config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 
 }
