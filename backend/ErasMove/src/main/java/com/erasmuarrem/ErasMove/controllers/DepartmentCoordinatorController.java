08e5c89b97067494ad157df5e9817b534eb66076
HP <alperengozeten@gmail.com>
2022-12-19 07:17:25 +0300

    comments

--- backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/controllers/DepartmentCoordinatorController.java
+++ backend/ErasMove/src/main/java/com/erasmuarrem/ErasMove/controllers/DepartmentCoordinatorController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * this is a controller class for managing the operations related to the department coordinator
+ */
 @RestController
 @RequestMapping("/departmentCoordinator")
 @CrossOrigin
@@ -19,27 +22,48 @@ public class DepartmentCoordinatorController {
         this.departmentCoordinatorService = departmentCoordinatorService;
     }
 
+    /**
+     * this method fetches all department coordinators from the database
+     * @return List<DepartmentCoordinator>
+     */
     @GetMapping()
     public List<DepartmentCoordinator> getDepartmentCoordinators() {
         return departmentCoordinatorService.getDepartmentCoordinators();
     }
 
+    /**
+     * this method fetches a department coordinator with a given id
+     * @param id Long
+     * @return DepartmentCoordinator
+     */
     @GetMapping("/{id}")
     public DepartmentCoordinator getDepartmentCoordinatorById(@PathVariable("id") Long id) {
         return departmentCoordinatorService.getDepartmentCoordinatorById(id);
     }
 
+    /**
+     * This method fetches a DepartmentCoordinator with a given departmentID
+     * @param id Long
+     * @return DepartmentCoordinator
+     */
     @GetMapping("/department/{id}")
     public DepartmentCoordinator getDepartmentCoordinatorByDepartmentId(@PathVariable("id") Long id) {
         return departmentCoordinatorService.getDepartmentCoordinatorByDepartmentId(id);
     }
 
+    /**
+     * this method adds a new Department Coordinator to the database
+     * @param departmentCoordinator DepartmentCoordinator
+     */
     @PostMapping("/add")
     public void addDepartmentCoordinator(@RequestBody DepartmentCoordinator departmentCoordinator) {
         departmentCoordinatorService.addDepartmentCoordinator(departmentCoordinator);
     }
 
-
+    /**
+     * this method deletes a department coordinator with a given id
+     * @param id Long
+     */
     @DeleteMapping("/delete/{id}")
     public void deleteDepartmentCoordinatorById(@PathVariable("id") Long id) {
         departmentCoordinatorService.deleteDepartmentCoordinatorById(id);
